ec3d6b578414eeb5622766b1fd611896a1ae240c
Denice Montemayor <dmontema@1-E-13.42heilbronn.de>
2021-12-10 02:02:15 +0100

    implement prototypes: server, client. able to send messages.

--- minitalk.c
+++ minitalk.c
@@ -6,7 +6,7 @@
 /*   By: dmontema <dmontema@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/07 22:51:50 by dmontema          #+#    #+#             */
-/*   Updated: 2021/12/09 22:12:52 by dmontema         ###   ########.fr       */
+/*   Updated: 2021/12/10 01:32:57 by dmontema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,21 @@
 
 // void handle_sigint(int sig)
 // {
-// 	if (sig == 30)
-// 		printf("Signal 1 received!\n", sig);
-// 	else if (sig == 31)
-// 		printf("Signal 2 received!\n", sig);
+// 	if (sig == SIGUSR1)
+// 		printf("Signal 1 received!\n");
+// 	else if (sig == SIGUSR2)
+// 		printf("Signal 2 received!\n");
 // }
 
-int bin_to_dec(char *bin)
+void bin_to_dec(char *bin)
 {
 	int res;
 	int help;
+	char c;
 
 	help = 128;
 	res = 0;
+	// printf("res bf:%d\n", res);
 	while (*bin)
 	{
 		if (*bin == '1')
@@ -37,53 +39,50 @@ int bin_to_dec(char *bin)
 		help /= 2;
 		bin++;
 	}
-	return (res);
+	c = res;
+	// printf("OK\n");
+	// printf("res af:%c\n", res);
+	// printf("%c ", res);
+	write(1, &c, 1);
+	return ;
+	// printf("%c", res);
 }
-
-void dec_to_bin(int val)
+void createBin(int sig)
 {
-	char *res;
-	int i;
+	static char res[9];
+	static int i;
+	// printf("call, i=%d\n", i);
 
-	i = 0;
-	res = calloc(8 + 1, sizeof(char));
-	while (i < 8)
+	if (sig == SIGUSR1)
+		res[i++] = '0';
+	else if (sig == SIGUSR2)
+		res[i++] = '1';
+	// printf("%s %d\n", res, i);
+	if (i == 8)
 	{
-		if ((128 & (val<<i)))
-			// write (1, "1", 1);
-			res[i] = '1';
-		else
-			// write(1, "0", 1);
-			res[i] = '0';
-		i++;
-	}
-	res[i] = 0;
-
-	printf("%s: %c\n", res, bin_to_dec(res));
-}
-
-void str_to_char(char *input)
-{
-	while (*input)
-	{
-		dec_to_bin(*input);
-		input++;
+		// res[i] = 0;
+		i = 0;
+		bin_to_dec(res);
+		*res = 0;
+		// printf("vals after print: i=%d, res=%s", i, res);
 	}
 }
 
 int main(void)
 {
-	str_to_char("Hallo 42Heilbronn!");
-	// int pid = getpid();
+	// str_to_char("Hallo 42Heilbronn!");
+	int pid = getpid();
 	// struct sigaction sa;
-	// printf("PID: %d\n", pid);
+	printf("PID: %d\n", pid);
 	// sa.sa_handler= &handle_sigint;
 	// sigaction(SIGUSR1, &sa, NULL);
 	// sigaction(SIGUSR2, &sa, NULL);
-	// while (1)
-	// {
-	// 	pause();
-	// }
+	signal(SIGUSR1, &createBin);
+	signal(SIGUSR2, &createBin);
+	while (1)
+	{
+		pause();
+	}
 
 	// printf("PID: %d\n", pid);
 	// signal(SIGINT, handle_sigint);
--- sender.c
+++ sender.c
@@ -6,7 +6,7 @@
 /*   By: dmontema <dmontema@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 15:39:11 by dmontema          #+#    #+#             */
-/*   Updated: 2021/12/09 19:11:15 by dmontema         ###   ########.fr       */
+/*   Updated: 2021/12/10 01:53:53 by dmontema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,61 @@
 #include <unistd.h>
 #include <signal.h>
 
+void sendChar(char *str, int pid)
+{
+	while (*str)
+	{
+		if (*str == '0')
+			kill(pid, SIGUSR1);
+		else if (*str == '1')
+			kill(pid, SIGUSR2);
+		str++;
+		usleep(100);
+	}
+}
+
+void dec_to_bin(int val, int pid)
+{
+	char *res;
+	int i;
+
+	i = 0;
+	res = calloc(8 + 1, sizeof(char));
+	while (i < 8)
+	{
+		if ((128 & (val<<i)))
+			// write (1, "1", 1);
+			res[i] = '1';
+		else
+			// write(1, "0", 1);
+			res[i] = '0';
+		i++;
+	}
+	sendChar(res, pid);
+	// printf("%s\n", res);
+	free(res);
+}
+
+void str_to_char(char *input, int pid)
+{
+	while (*input)
+	{
+		dec_to_bin(*input, pid);
+		input++;
+	}
+}
+
 int main(int argc, char **argv)
 {
 	if (argc == 3)
 	{
 		int pid = atoi(argv[1]);
-		if (atoi(argv[2]) == 0)
-			kill(pid, SIGUSR1);
-		else
-			kill(pid, SIGUSR2);
-		usleep(500);
+		str_to_char(argv[2], pid);
+		printf("finished\n");
+		// if (atoi(argv[2]) == 0)
+		// 	kill(pid, SIGUSR1);
+		// else
+		// 	kill(pid, SIGUSR2);
+		// usleep(500);
 	}
 }
